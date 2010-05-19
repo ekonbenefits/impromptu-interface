0275f6e5c3ba53e8383349516054893bafcf9409
jayt <none@none>
2010-05-19 14:42:32 -0500

    Added some comments

--- QuackInterface/ActsLike.cs
+++ QuackInterface/ActsLike.cs
@@ -4,6 +4,9 @@ namespace QuackInterface
 {
     using System;
 
+    /// <summary>
+    /// This interface can be used on your custom dynamic objects if you want impromptu interfaces
+    /// </summary>
     public interface IActsLike
     {
         TInterface ActsLike<TInterface>(params Type[] otherInterfaces);
@@ -11,6 +14,13 @@ namespace QuackInterface
 
     public static class ObjectExtensions
     {
+        /// <summary>
+        /// Wraps an existing object with an Explicit interface definition
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <param name="originalDynamic">The original object can be annoymous type, System.DynamicObject as well as any others.</param>
+        /// <param name="otherInterfaces">The other interfaces.</param>
+        /// <returns></returns>
         public static TInterface ActsLike<TInterface>(this Object originalDynamic, params Type[]otherInterfaces)where TInterface:class
         {
             var tType = originalDynamic.GetType();
@@ -23,6 +33,13 @@ namespace QuackInterface
 
     public static class Dynamic
     {
+        /// <summary>
+        /// When using this method you must explicitly type the result otherwise the compiler will infer it as dynamic.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <param name="originalDynamic">The original dynamic object.</param>
+        /// <param name="otherInterfaces">The other interfaces.</param>
+        /// <returns></returns>
         public static TInterface ActsLike<TInterface>(dynamic originalDynamic, params Type[] otherInterfaces) where TInterface : class
         {
             var tType = originalDynamic.GetType();

2e496d59c723d56ec9c6d72db76c491dc1a92901
jayt <none@none>
2010-08-27 07:46:54 -0500

    Added impromptu dictionary

---
+++ ImpromptuInterface/Dynamic/ImpromptuDictionary.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace ImpromptuInterface.Dynamic
+{
+    public class ImpromptuDictionary:ImpromptuObject,IDictionary<string,object>
+    {
+        protected readonly IDictionary<string,object> _dictionary = new Dictionary<string, object>();
+
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if (_dictionary.ContainsKey(binder.Name))
+                result = _dictionary[binder.Name];
+            else
+            {
+                result = null;
+                Type tType;
+                if (!TryTypeForName(binder.Name, out tType))
+                {
+                    
+                    return false;
+                }
+                if (tType.IsValueType)
+                {
+                    result = Activator.CreateInstance(tType);
+                }
+            }
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _dictionary[binder.Name] = value;
+            return true;
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+           return _dictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<string, object> item)
+        {
+           _dictionary.Add(item);
+        }
+
+        public void Clear()
+        {
+            _dictionary.Clear();
+        }
+
+        public bool Contains(KeyValuePair<string, object> item)
+        {
+            return _dictionary.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
+        {
+            _dictionary.CopyTo(array,arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<string, object> item)
+        {
+            return _dictionary.Remove(item);
+        }
+
+        public int Count
+        {
+            get { return _dictionary.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _dictionary.IsReadOnly; }
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+
+        public void Add(string key, object value)
+        {
+            _dictionary.Add(key,value);
+        }
+
+        public bool Remove(string key)
+        {
+           return _dictionary.Remove(key);
+        }
+
+        public bool TryGetValue(string key, out object value)
+        {
+            return _dictionary.TryGetValue(key, out value);
+        }
+
+        public object this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+
+        public ICollection<string> Keys
+        {
+            get { return _dictionary.Keys; }
+        }
+
+        public ICollection<object> Values
+        {
+            get { return _dictionary.Values; }
+        }
+    }
+}

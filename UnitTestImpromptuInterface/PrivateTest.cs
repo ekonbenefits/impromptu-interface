d67fa702b848d51f14c33d2f80ea6356e7fcb8c5
jay@vala.local <jay@vala.local>
2010-08-30 21:06:57 -0500

    Added License information reorg'd unit tests

---
+++ UnitTestImpromptuInterface/PrivateTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImpromptuInterface;
+using ImpromptuInterface.Dynamic;
+using NUnit.Framework;
+
+namespace TestImpromtuInterface
+{
+
+
+
+  
+
+    [TestFixture]
+    public class PrivateTest:AssertionHelper
+    {
+        [Test]
+        public void Test()
+        {
+			
+			
+            var tTest =new TestWithPrivateMethod();
+
+            //tTest.Test(); //Doesn't work cuz it's private
+
+			
+            var tExposed =tTest.ActLike<IExposePrivateMethod>();
+
+            Assert.AreEqual(3,tExposed.Test());//Works
+        }
+		
+
+    }
+	
+		 public class TestWithPrivateMethod
+    {
+        private int Test()
+        {
+            return 3;
+        }
+    }
+	
+	public interface  IExposePrivateMethod
+    {
+        int Test();
+    }
+}

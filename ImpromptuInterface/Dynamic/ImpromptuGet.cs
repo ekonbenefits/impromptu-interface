6ffa18066a999994b0e6c802ff2505aec0cdb808
jayt <none@none>
2011-03-04 21:51:56 -0600

    Refactored inline lambda classes into new file.
    --HG--
    branch : dev

--- ImpromptuInterface/Dynamic/ImpromptuGet.cs
+++ ImpromptuInterface/Dynamic/ImpromptuGet.cs
@@ -63,25 +63,4 @@ namespace ImpromptuInterface.Dynamic
         }
     }
 
-    public static class Return<TR>
-    {
-        public static Func<TR> Arguments(Func<TR> del)
-        {
-            return del;
-        }
-
-        public static Func<T1, TR> Arguments<T1>(Func<T1, TR> del)
-        {
-            return del;
-        }
-
-    }
-
-    public static class Void
-    {
-        public static Action Arguments(Action del)
-        {
-            return del;
-        }
-    }
 }
---
+++ ImpromptuInterface/Dynamic/InlineLambda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImpromptuInterface.Dynamic
+{
+    public static class Return<TR>
+    {
+        public static Func<TR> Arguments(Func<TR> del)
+        {
+            return del;
+        }
+
+        public static Func<T1, TR> Arguments<T1>(Func<T1, TR> del)
+        {
+            return del;
+        }
+
+    }
+
+    public static class Void
+    {
+        public static Action Arguments(Action del)
+        {
+            return del;
+        }
+    }
+}

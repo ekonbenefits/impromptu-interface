d05d421de2227a057591bb6492b8552899535182
jayt <none@none>
2011-04-05 09:57:48 -0500

    Added pure Dynamic MVVM view model

---
+++ ImpromptuInterface.MVVM/ImpromptuRelayCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using ImpromptuInterface;
+
+namespace ImpromptuInterface.MVVM
+{
+
+
+    /// <summary>
+    /// Command that relays to a target and method name.
+    /// </summary>
+    public class ImpromptuRelayCommand : ICommand
+    {
+        private readonly object _executeTarget;
+        private readonly string _executeName;
+        private readonly object _canExecuteTarget;
+        private readonly string _canExecuteName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImpromptuRelayCommand"/> class.
+        /// </summary>
+        /// <param name="executeTarget">The execute target.</param>
+        /// <param name="executeName">Name of the execute.</param>
+        public ImpromptuRelayCommand(object executeTarget, string executeName)
+        {
+            _executeTarget = executeTarget;
+            _executeName = executeName;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImpromptuRelayCommand"/> class.
+        /// </summary>
+        /// <param name="executeTarget">The execute target.</param>
+        /// <param name="executeName">Name of the execute method.</param>
+        /// <param name="canExecuteTarget">The can execute target.</param>
+        /// <param name="canExecuteName">Name of the can execute method.</param>
+        public ImpromptuRelayCommand(object executeTarget, string executeName, object canExecuteTarget, string canExecuteName)
+        {
+            _executeTarget = executeTarget;
+            _executeName = executeName;
+            _canExecuteTarget = canExecuteTarget;
+            _canExecuteName = canExecuteName;
+        }
+
+        public void Execute(object parameter)
+        {
+            Impromptu.InvokeMemberAction(_executeTarget, _executeName, parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteTarget == null)
+                return true;
+            return Impromptu.InvokeMember(_canExecuteTarget, _canExecuteName, parameter);
+        }
+
+
+#if !SILVERLIGHT   
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+#else
+        public event EventHandler CanExecuteChanged;
+#endif
+
+
+    }
+}
---
+++ ImpromptuInterface.MVVM/ImpromptuViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using ImpromptuInterface.Dynamic;
+
+namespace ImpromptuInterface.MVVM
+{
+    /// <summary>
+    /// View Model that uses a Dynamic Implementation to remove boilerplate for Two-Way bound properties and commands to methods
+    /// </summary>
+    public class ImpromptuViewModel:ImpromptuDictionary
+    {
+        private Trampoline _trampoline;
+
+        /// <summary>
+        /// Convenient access to Dynamic Properties. When subclassing you can use Dynamic.PropertyName = x, etc.
+        /// </summary>
+        /// <value>The command.</value>
+        protected dynamic Dynamic
+        {
+            get { return this; }
+        }
+
+        /// <summary>
+        /// Gets the command for binding. usage: {Binding Command.MethodName} for <code>void MethodName(object parmeter)</code> and optionally <code>bool CanMethodName(object parameter)</code>.
+        /// </summary>
+        /// <value>The command.</value>
+        public virtual dynamic Command
+        {
+            get { return _trampoline ?? (_trampoline = new Trampoline(this)); }
+        }
+
+        protected class Trampoline : ImpromptuDictionary
+        {
+            private readonly ImpromptuDictionary _parent;
+
+            public Trampoline(ImpromptuDictionary parent)
+            {
+                _parent = parent;
+            }
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                if (!base.TryGetMember(binder, out result))
+                {
+                    var tName = binder.Name;
+                    var tCanExecute = "Can" + tName;
+                    if (_parent.ContainsKey(tCanExecute) || _parent.GetType().GetMethod(tCanExecute) != null)
+                    {
+                        result = new ImpromptuRelayCommand(_parent, tName, _parent, tCanExecute);
+                    }
+                    else
+                    {
+                        result = new ImpromptuRelayCommand(_parent, tName);
+                    }
+                    this[tName] = result;
+                }
+                return true;
+            }
+        }
+
+    
+    }
+}

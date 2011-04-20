3ccb044c55f394fe9f69ab5d400b7bf04f62af67
jayt <none@none>
2011-04-20 16:20:09 -0500

    changed ifdef to .net/silerlight agnostic, added comments.
    --HG--
    branch : dev

--- ImpromptuInterface.MVVM/Event.cs
+++ ImpromptuInterface.MVVM/Event.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿// 
+//  Copyright 2011 Ekon Benefits
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
@@ -54,18 +69,7 @@ namespace ImpromptuInterface.MVVM
 
 
        
-#if!SILVERLIGHT
 
-        /// <summary>
-        /// Property to Describe binding Events in DataContext
-        /// </summary>
-        public static readonly DependencyProperty BindProperty = DependencyProperty.RegisterAttached(
-          "Bind",
-          typeof(EventBinder),
-          typeof(Event),
-          new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender, OnBindChange)
-        );
-#else
         /// <summary>
         /// Property to Describe binding Events in DataContext
         /// </summary>
@@ -76,7 +80,6 @@ namespace ImpromptuInterface.MVVM
                                                                                                      new PropertyMetadata
                                                                                                          (null,
                                                                                                           OnBindChange));
-#endif
 
 
         private static void OnBindChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -93,11 +96,21 @@ namespace ImpromptuInterface.MVVM
             }
         }
 
+        /// <summary>
+        /// Sets the bind.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="value">The value.</param>
         public static void SetBind(DependencyObject element, EventBinder value)
         {
             element.SetValue(BindProperty, value);
         }
 
+        /// <summary>
+        /// Gets the bind.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <returns></returns>
         public static EventBinder GetBind(DependencyObject element)
         {
             return element.GetValue(BindProperty) as EventBinder;
@@ -112,19 +125,34 @@ namespace ImpromptuInterface.MVVM
             return (sender, e) => func(sender, e);
         }
 
+        /// <summary>
+        /// Object with method to create latebound eventhandler delegates
+        /// </summary>
         public class BinderEventHandlerMemberName
         {
 
+            /// <summary>
+            /// The Invoke Method's Info
+            /// </summary>
             public static readonly MethodInfo InvokeMethodInfo =
                 typeof (BinderEventHandlerMemberName).GetMethod("Invoke");
 
             private readonly string _name;
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="BinderEventHandlerMemberName"/> class.
+            /// </summary>
+            /// <param name="name">The name.</param>
             public BinderEventHandlerMemberName(string name)
             {
                 _name = name;
             }
 
+            /// <summary>
+            /// Invokes the eventhandler
+            /// </summary>
+            /// <param name="sender">The sender.</param>
+            /// <param name="e">The e.</param>
             public void Invoke(object sender, object e)
             {
                  var tSender = sender as DependencyObject;
@@ -148,6 +176,12 @@ namespace ImpromptuInterface.MVVM
             }
         }
 
+        /// <summary>
+        /// Generates the event handler.
+        /// </summary>
+        /// <param name="delType">Type of the del.</param>
+        /// <param name="membername">The membername.</param>
+        /// <returns></returns>
         public static object GenerateEventHandler(Type delType, string membername)
         {
             var tHash = new EventHandlerHash(membername, delType);
@@ -168,6 +202,9 @@ namespace ImpromptuInterface.MVVM
      
     }
 
+    /// <summary>
+    /// Helper Object to create event bindings.
+    /// </summary>
     public class EventBinder:DynamicObject
     {
         public object Target { get; protected set; }
@@ -182,6 +219,10 @@ namespace ImpromptuInterface.MVVM
         }
 
 
+        /// <summary>
+        /// Registers the events on the  specified source.
+        /// </summary>
+        /// <param name="source">The source.</param>
         public void Register(Object source)
         {
             foreach (var tPair in List)
@@ -190,6 +231,10 @@ namespace ImpromptuInterface.MVVM
             }
         }
 
+        /// <summary>
+        /// Unregisters the events on the specified source.
+        /// </summary>
+        /// <param name="source">The source.</param>
         public void UnRegister(Object source)
         {
             foreach (var tPair in List)
@@ -198,8 +243,15 @@ namespace ImpromptuInterface.MVVM
             }
         }
 
-        
 
+
+        /// <summary>
+        /// Registers or unregister.
+        /// </summary>
+        /// <param name="un">if set to <c>true</c> [un].</param>
+        /// <param name="source">The source.</param>
+        /// <param name="eventName">Name of the event.</param>
+        /// <param name="targetName">Name of the target.</param>
         private void RegisterUnRegister(bool un,object source, string eventName, string targetName)
         {
             if (Impromptu.InvokeIsEvent(source, eventName))
@@ -222,6 +274,10 @@ namespace ImpromptuInterface.MVVM
             return true;
         }
 
+        /// <summary>
+        /// To is used to specify what the event is going to bind to
+        /// </summary>
+        /// <value>To.</value>
         public virtual EventBinderTo To
         {
             get { return Child.Value; }
@@ -232,6 +288,10 @@ namespace ImpromptuInterface.MVVM
             List[_lastKey] = value;
         }
 
+        /// <summary>
+        /// Gets the <see cref="System.Object"/> at the specified index.
+        /// </summary>
+        /// <value></value>
         public virtual object this[string index]
         {
             get
@@ -242,15 +302,31 @@ namespace ImpromptuInterface.MVVM
             }
         }
 
+        /// <summary>
+        /// Helper Object to create event bindings.
+        /// </summary>
         public class EventBinderTo : EventBinder
         {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="EventBinder.EventBinderTo"/> class.
+            /// </summary>
+            /// <param name="target">The target.</param>
+            /// <param name="parent">The parent.</param>
             public EventBinderTo(object target, EventBinder parent) : base(target)
             {
                 Parent = parent;
             }
 
+            /// <summary>
+            /// Gets or sets the parent.
+            /// </summary>
+            /// <value>The parent.</value>
             public EventBinder Parent { get; protected set; }
 
+            /// <summary>
+            /// Gets the <see cref="System.Object"/> at the specified index.
+            /// </summary>
+            /// <value></value>
             public override object this[string index]
             {
                 get

bd3c9c7a16671a8f4b12b37cb1e186fb208abe50
jayt <none@none>
2011-04-20 08:28:19 -0500

    updated version numbers
    --HG--
    branch : dev

--- ImpromptuInterface/Properties/AssemblyInfo.cs
+++ ImpromptuInterface/Properties/AssemblyInfo.cs
@@ -54,7 +54,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 #if SILVERLIGHT
-[assembly: AssemblyVersion("4.0.7.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
 #else
-[assembly: AssemblyVersion("4.0.7.*")]
+[assembly: AssemblyVersion("4.1.0.*")]
 #endif

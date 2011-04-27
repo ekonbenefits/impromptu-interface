040994e1fb7b7b81ceee683ad33f482c9651c111
jayt <none@none>
2011-04-27 11:43:20 -0500

    fixed build file issue.
    --HG--
    branch : dev

--- ImpromptuInterface.MVVM/Properties/AssemblyInfo.cs
+++ ImpromptuInterface.MVVM/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 #if SILVERLIGHT
-[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyVersion("3.0.3.0")]
 #else
-[assembly: AssemblyVersion("3.0.1.*")]
+[assembly: AssemblyVersion("3.0.3.*")]
 #endif

ef4dfa1fd56671c93606a82b5ce05cacd1dde1f6
jayt <none@none>
2011-04-29 07:23:53 -0500

    updated version and icon

--- ImpromptuInterface.MVVM/Properties/AssemblyInfo.cs
+++ ImpromptuInterface.MVVM/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 #if SILVERLIGHT
-[assembly: AssemblyVersion("3.0.3.0")]
+[assembly: AssemblyVersion("3.0.4.0")]
 #else
-[assembly: AssemblyVersion("3.0.3.*")]
+[assembly: AssemblyVersion("3.0.4.*")]
 #endif

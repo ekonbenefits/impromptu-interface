b15954a35805513a141246b756067afd7ef543b9
jayt <none@none>
2011-06-28 09:18:58 -0500

    Some property info updates.

--- ImpromptuInterface.MVVM/Properties/AssemblyInfo.cs
+++ ImpromptuInterface.MVVM/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 #if SILVERLIGHT
-[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyVersion("3.1.2.0")]
 #else
-[assembly: AssemblyVersion("3.1.1.*")]
+[assembly: AssemblyVersion("3.1.2.*")]
 #endif

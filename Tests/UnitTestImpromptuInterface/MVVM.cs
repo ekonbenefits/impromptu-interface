4e24687fea102c79e56399faf865a102234b2fd9
jonathan.peppers <none@none>
2011-09-17 18:25:57 -0500

    Added IoC-classes from NoMvvm Added MEF project and first MEFTest

--- Tests/UnitTestImpromptuInterface/MVVM.cs
+++ Tests/UnitTestImpromptuInterface/MVVM.cs
@@ -25,29 +25,29 @@ namespace UnitTestImpromptuInterface
 #endif
 {
 
-       
+
     [TestFixture]
-	public class MVVM:Helper
-	{
+    public class MVVM : Helper
+    {
 
-         [Test]
+        [Test]
         public void TestToStringProxy()
-         {
-             dynamic tProxy =
-                 new {Test1 = "One", Test2 = "Two", TestAgain = "Again"}.ProxyToString(
-                     it => string.Format("{0}:{1}", it.Test1, it.TestAgain));
+        {
+            dynamic tProxy =
+                new { Test1 = "One", Test2 = "Two", TestAgain = "Again" }.ProxyToString(
+                    it => string.Format("{0}:{1}", it.Test1, it.TestAgain));
 
-             Assert.AreEqual("One:Again",tProxy.ToString());
-         }
+            Assert.AreEqual("One:Again", tProxy.ToString());
+        }
 
-         [Test]
-         public void TestResultToStringProxyDictionary()
-         {
+        [Test]
+        public void TestResultToStringProxyDictionary()
+        {
 
-             object tTarget =
-                 new {Test1 = "One", Test2 = 5.24d, TestAgain = 5, TestFinal = new List<string>()};
+            object tTarget =
+                new { Test1 = "One", Test2 = 5.24d, TestAgain = 5, TestFinal = new List<string>() };
 
-             dynamic tProxy = new ImpromptuResultToString(tTarget)
+            dynamic tProxy = new ImpromptuResultToString(tTarget)
                              {
                                  new Func<int, string>(it => "int"),
                                  new Func<double, string>(it => "double"),
@@ -55,85 +55,85 @@ namespace UnitTestImpromptuInterface
                                   new Func<IList<string>, string>(it => "List"),
                              };
 
-             Assert.AreEqual("string", tProxy.Test1.ToString());
-             Assert.AreEqual("double", tProxy.Test2.ToString());
-             Assert.AreEqual("int", tProxy.TestAgain.ToString());
-             Assert.AreEqual("List", tProxy.TestFinal.ToString());
-         }
+            Assert.AreEqual("string", tProxy.Test1.ToString());
+            Assert.AreEqual("double", tProxy.Test2.ToString());
+            Assert.AreEqual("int", tProxy.TestAgain.ToString());
+            Assert.AreEqual("List", tProxy.TestFinal.ToString());
+        }
 
 
-         [Test]
-         public void TestToStringProxyCall()
-         {
-             var tAnon = new PropPoco() { Prop1 = "A", Prop2 = 1 };
+        [Test]
+        public void TestToStringProxyCall()
+        {
+            var tAnon = new PropPoco() { Prop1 = "A", Prop2 = 1 };
 
-             dynamic tProxy = tAnon.ProxyToString(
-                     it => string.Format("{0}:{1}", it.Prop1, it.Prop2));
-
-
-             Assert.AreEqual(tAnon.Prop2, tProxy.Prop2);
-         }
+            dynamic tProxy = tAnon.ProxyToString(
+                    it => string.Format("{0}:{1}", it.Prop1, it.Prop2));
 
 
-         [Test]
+            Assert.AreEqual(tAnon.Prop2, tProxy.Prop2);
+        }
+
+
+        [Test]
         public void TestToStringProxyConvertExplicit()
-         {
-             var tAnon = new {Test1 = "One", Test2 = "Two", TestAgain = "Again"};
+        {
+            var tAnon = new { Test1 = "One", Test2 = "Two", TestAgain = "Again" };
 
-             dynamic tProxy = tAnon.ProxyToString(
-                     it => string.Format("{0}:{1}", it.Test1, it.TestAgain));
+            dynamic tProxy = tAnon.ProxyToString(
+                    it => string.Format("{0}:{1}", it.Test1, it.TestAgain));
 
-             var tAnon2 = ExplictAnonCast(tAnon, tProxy);
-             Assert.AreEqual(tAnon, tAnon2);
-         }
-
-
-
-         [Test]
-         public void TestToStringProxyConvertImplicit()
-         {
-             var tAnon = new PropPoco(){Prop1 = "A", Prop2 = 1};
-
-             dynamic tProxy = tAnon.ProxyToString(
-                     it => string.Format("{0}:{1}", it.Prop1, it.Prop2));
-
-             var tAnon2 = ImplicitCast(tProxy);
-             Assert.AreEqual(tAnon.GetType(), tAnon2.GetType());
-         }
-
-         [Test]
-         public void TestToStringProxyConvertImplicit2()
-         {
-             var tAnon = new PropPoco() { Prop1 = "A", Prop2 = 1 };
-
-             dynamic tProxy = tAnon.ProxyToString(
-                     it => string.Format("{0}:{1}", it.Prop1, it.Prop2));
-
-             var tAnon2 = ImplicitCastDynamic(tProxy);
-             Assert.AreEqual(tAnon.GetType(), tAnon2.GetType());
-         }
-        
-        
-       
+            var tAnon2 = ExplictAnonCast(tAnon, tProxy);
+            Assert.AreEqual(tAnon, tAnon2);
+        }
 
 
-         [Test]
-         public void TestToStringProxyDictionaryMassage()
-         {
-             var tPropPoco = new PropPoco() {Prop1 = "A"};
 
-             dynamic tData = Builder.New().Object(ReturnProp: tPropPoco.ProxyToString(it=>"Print Me"));
+        [Test]
+        public void TestToStringProxyConvertImplicit()
+        {
+            var tAnon = new PropPoco() { Prop1 = "A", Prop2 = 1 };
 
-             IPropPocoProp tInter =Impromptu.ActLike<IPropPocoProp>(tData);
+            dynamic tProxy = tAnon.ProxyToString(
+                    it => string.Format("{0}:{1}", it.Prop1, it.Prop2));
 
-             Assert.AreEqual(tPropPoco.GetType(), tInter.ReturnProp.GetType());
-         }
+            var tAnon2 = ImplicitCast(tProxy);
+            Assert.AreEqual(tAnon.GetType(), tAnon2.GetType());
+        }
 
-     
+        [Test]
+        public void TestToStringProxyConvertImplicit2()
+        {
+            var tAnon = new PropPoco() { Prop1 = "A", Prop2 = 1 };
+
+            dynamic tProxy = tAnon.ProxyToString(
+                    it => string.Format("{0}:{1}", it.Prop1, it.Prop2));
+
+            var tAnon2 = ImplicitCastDynamic(tProxy);
+            Assert.AreEqual(tAnon.GetType(), tAnon2.GetType());
+        }
+
+
+
+
+
+        [Test]
+        public void TestToStringProxyDictionaryMassage()
+        {
+            var tPropPoco = new PropPoco() { Prop1 = "A" };
+
+            dynamic tData = Builder.New().Object(ReturnProp: tPropPoco.ProxyToString(it => "Print Me"));
+
+            IPropPocoProp tInter = Impromptu.ActLike<IPropPocoProp>(tData);
+
+            Assert.AreEqual(tPropPoco.GetType(), tInter.ReturnProp.GetType());
+        }
+
+
 
         private T ExplictAnonCast<T>(T dummy, dynamic value)
         {
-            return (T) value;
+            return (T)value;
         }
 
         private PropPoco ImplicitCast(PropPoco value)
@@ -145,14 +145,14 @@ namespace UnitTestImpromptuInterface
             return value;
         }
 
-        internal class TestDependency:DependencyObject
+        internal class TestDependency : DependencyObject
         {
 
 
             public event TextChangedEventHandler TextChange;
 
 
-            public void OnTextChanged(object sender,TextChangedEventArgs e)
+            public void OnTextChanged(object sender, TextChangedEventArgs e)
             {
                 if (TextChange != null)
                 {
@@ -182,8 +182,8 @@ namespace UnitTestImpromptuInterface
             var tViewModel = Build<ImpromptuViewModel>.NewObject(TestCommand: new Action<object>((arg) => tRun = true));
 
 
-            ICommand command =tViewModel.Command.TestCommand;
-            
+            ICommand command = tViewModel.Command.TestCommand;
+
             command.Execute(null);
 
             Assert.AreEqual(true, tRun);
@@ -198,7 +198,7 @@ namespace UnitTestImpromptuInterface
                                                                                                                         throw
                                                                                                                             new Exception
                                                                                                                                 ("Test");
-                                                                                                                        
+
                                                                                                                     }));
             Action<Exception> onError = (ex) => tRun = true;
             tViewModel.Setup.CommandErrorHandler += onError;
@@ -228,7 +228,7 @@ namespace UnitTestImpromptuInterface
         public void TestCommandBindingCanProperty()
         {
             var tRun = false;
-            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestCommand: new Action(() => tRun = true) ,CanTestCommand:false);
+            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestCommand: new Action(() => tRun = true), CanTestCommand: false);
 
 
             ICommand command = tViewModel.Command.TestCommand;
@@ -241,7 +241,7 @@ namespace UnitTestImpromptuInterface
         public void TestCommandBindingCanNoArg()
         {
             var tRun = false;
-            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestCommand: new Action(() => tRun = true), CanTestCommand: new Func<bool>(()=>false));
+            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestCommand: new Action(() => tRun = true), CanTestCommand: new Func<bool>(() => false));
 
 
             ICommand command = tViewModel.Command.TestCommand;
@@ -255,7 +255,7 @@ namespace UnitTestImpromptuInterface
         public void TestCommandBindingCanArg()
         {
             var tRun = false;
-            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestCommand: new Action(() => tRun = true), CanTestCommand: new Func<object,bool>((param) => false));
+            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestCommand: new Action(() => tRun = true), CanTestCommand: new Func<object, bool>((param) => false));
 
 
             ICommand command = tViewModel.Command.TestCommand;
@@ -265,15 +265,12 @@ namespace UnitTestImpromptuInterface
             Assert.AreEqual(false, tCanCommand);
         }
 
-
-   
-   
         [Test]
         public void TestEventBinding()
         {
             var tRun = false;
             var tTextBox = new TestDependency();
-            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestEvent:new Action<object,EventArgs>((sender,e)=>tRun =true));
+            var tViewModel = Build<ImpromptuViewModel>.NewObject(TestEvent: new Action<object, EventArgs>((sender, e) => tRun = true));
 
 
             Event.SetBind(tTextBox, tViewModel.Events.TextChange.To["TestEvent"]);
@@ -284,16 +281,14 @@ namespace UnitTestImpromptuInterface
         }
 
 
-             [Test]
-         public void TestTypeConverter()
-             {
-                 object tNew = Build.NewObject(ColorViaString: "Blue");
-                var tColorHolder = tNew.ActLike<IColor>();
+        [Test]
+        public void TestTypeConverter()
+        {
+            object tNew = Build.NewObject(ColorViaString: "Blue");
+            var tColorHolder = tNew.ActLike<IColor>();
 
-                Assert.AreEqual(Colors.Blue, tColorHolder.ColorViaString);
-
-
-             }
+            Assert.AreEqual(Colors.Blue, tColorHolder.ColorViaString);
+        }
 
         [Test]
         public void TestOnChangeDependencyCalls()
@@ -307,8 +302,8 @@ namespace UnitTestImpromptuInterface
             int tEvent1Count = 0;
             int tEvent2Count = 0;
 
-            var tEvent1 =ImpromptuViewModel.ChangedHandler((sender, e) => tEvent1Count++);
-            Action<object,EventArgs> tEvent2Func = (sender, e) => tEvent2Count++;
+            var tEvent1 = ImpromptuViewModel.ChangedHandler((sender, e) => tEvent1Count++);
+            Action<object, EventArgs> tEvent2Func = (sender, e) => tEvent2Count++;
             var tEvent2 = new PropertyChangedEventHandler(tEvent2Func);
             var tEvent2Again = new PropertyChangedEventHandler(tEvent2Func);
 
@@ -319,12 +314,12 @@ namespace UnitTestImpromptuInterface
 
             tNewViewModel.Prop1 = "Run";
 
-            Assert.AreEqual(1,tEvent1Count);
+            Assert.AreEqual(1, tEvent1Count);
 
             Assert.AreEqual(1, tEvent2Count);
-        
+
         }
 
-     
-	}
+        
+    }
 }
